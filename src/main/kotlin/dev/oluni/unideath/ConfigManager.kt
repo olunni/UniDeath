aec3011b2160b18243598c27d2c615b760429d9d
olunni <oluniofficial@gmail.com>
2026-04-24 22:04:37 +0300

    feat: add messages for future command

--- src/main/kotlin/dev/oluni/unideath/ConfigManager.kt
+++ src/main/kotlin/dev/oluni/unideath/ConfigManager.kt
@@ -6,4 +6,15 @@ class ConfigManager(val plugin: UniDeath) {
 
     val instantRespawnEnabled: Boolean
         get() = config.getBoolean("instant-respawn", true)
+
+    val usageMessage: String
+        get() = config.getString("messages.usage") ?: "<gradient:#FFA7D9:#FFA7D9>UniDeath</gradient> <gray>-> <red>Usage: /unideath reload"
+    val reloadSuccessfulMessage: String
+        get() = config.getString("messages.successful-reload") ?: "<gradient:#FFA7D9:#FFA7D9>UniDeath</gradient> <gray>-> <green>Config reload successful!</green>"
+    val noPermissionMessage: String
+        get() = config.getString("messages.successful-reload") ?: "<gradient:#FFA7D9:#FFA7D9>UniDeath</gradient> <gray>-> <red>You don't have permission to do this!"
+
+    fun reloadConfig() {
+        plugin.reloadConfig()
+    }
 }
